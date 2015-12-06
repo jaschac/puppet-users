4fcd80b0664b69f45a1148966390f358ca8d1f90
jascha <jaschacasadio@gmail.com>
2015-12-06 05:02:12 +0100

    Merged mandatory and extra dependencies

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,26 +1,28 @@
 # Install the dependencies of lostinmalloc-users
 class users::install{
-  if $::users::mandatory_dependencies {
-    $::users::mandatory_dependencies.each |$dependency,$provider| {
-      if !defined(Package[$dependency]) {
-        Package { $dependency:
-          ensure   => $::users::package_ensure,
-	  provider => $provider,
-	  tag      => 'mandatory_dependency',
-        }
-      }
-    }
+
+  $mandatory_dependencies = empty($::users::mandatory_dependencies) ? {
+    false => $::users::mandatory_dependencies,
+    true  => {},
   }
 
-  if $::users::extra_dependencies {
-    $::users::extra_dependencies.each |$dependency,$provider| {
+  $extra_dependencies = empty($::users::extra_dependencies) ? {
+    false => $::users::extra_dependencies,
+    true  => {},
+  }
+
+  $dependencies = merge($mandatory_dependencies, $extra_dependencies)
+
+  if !empty($dependencies){
+    $dependencies.each |$dependency,$provider| {
       if !defined(Package[$dependency]) {
         Package { $dependency:
           ensure   => $::users::package_ensure,
           provider => $provider,
-          tag      => 'extra_dependency',
-        }
-      }
+          tag      => 'dependency',
+        }   
+      }   
     }
   }
+
 }
